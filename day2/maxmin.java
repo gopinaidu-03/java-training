9fd71a824d5d33884089dd93a33520255ba796b0
gopinaidu-03 <gajulavenkatagopi@gmail.com>
2026-08-16 23:05:53 +0530

    Add files via upload

---
+++ day2/maxmin.java
@@ -0,0 +1,8 @@
+class maxmin
+{
+    public static void main(String args[])
+    {
+        System.out.println("Maximum Integer: " + Integer.MAX_VALUE);
+        System.out.println("Minimum Integer: " + Integer.MIN_VALUE);
+    }
+}
