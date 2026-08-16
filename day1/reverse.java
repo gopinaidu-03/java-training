7977f4b85590602d7b34f085d02d6bc64864083b
gopinaidu-03 <gajulavenkatagopi@gmail.com>
2026-08-16 23:04:45 +0530

    Add files via upload

---
+++ day1/reverse.java
@@ -0,0 +1,16 @@
+class reverse {
+    public static void main(String args[])
+    {
+       int no = 12345;
+       System.out.print(no % 10);
+       no = no / 10;
+       System.out.print(no % 10);
+         no = no / 10;
+         System.out.print(no % 10);
+         no = no / 10;
+         System.out.print(no % 10);
+         no = no / 10;
+         System.out.print(no % 10);
+        
+    }
+}
